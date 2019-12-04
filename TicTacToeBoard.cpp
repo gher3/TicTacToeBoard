b1401ce81a07d7868eab9373e7d346f0136f54ef
Gary Her <gher3@mail.csuchico.edu>
2019-12-04 15:44:58 -0800

    Fixed getWinner function

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -81,29 +81,53 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 **/
 Piece TicTacToeBoard::getWinner()
 {
+  //first row
   if(board[0][0] == board[0][1] && board[0][0] == board[0][2]){
-    return board[0][0];
+    if(board[0][0] == X || board[0][0] == O){
+      return board[0][0];
+    }
   }
+  //back slash
   if(board[0][0] == board[1][1] && board[0][0] == board[2][2]){
-    return board[0][0];
+    if(board[0][0] == X || board[0][0] == O){
+      return board[0][0];
+    }
   }
+  //first column
   if(board[0][0] == board[1][0] && board[0][0] == board[2][0]){
-    return board[0][0];
+    if(board[0][0] == X || board[0][0] == O){
+      return board[0][0];
+    }
   }
+  //second row
   if(board[1][0] == board[1][1] && board[1][0] == board[1][2]){
-    return board[1][0];
+    if(board[1][0] == X || board[1][0] == O){
+      return board[1][0];
+    }
   }
+  //third row
   if(board[2][0] == board[2][1] && board[2][0] == board[2][2]){
-    return board[2][0];
+    if(board[2][0] == X || board[2][0] == O){
+      return board[2][0];
+    }
   }
-  if(board[1][0] == board[1][1] && board[1][0] == board[1][2]){
-    return board[1][0];
+  //second column
+  if(board[0][1] == board[1][1] && board[0][1] == board[2][1]){
+    if(board[0][1] == X || board[0][1] == O){
+      return board[0][1];
+    }
   }
-  if(board[2][0] == board[2][1] && board[2][0] == board[2][2]){
-    return board[2][0];
+  //third column
+  if(board[0][2] == board[1][2] && board[0][2] == board[2][2]){
+    if(board[0][2] == X || board[0][2] == O){
+      return board[0][2];
+    }
   }
-  if(board[2][0] == board[1][1] && board[2][0] == board[0][2]){
-    return board[2][0];
+  //front slash
+  if(board[0][2] == board[1][1] && board[0][2] == board[2][0]){
+    if(board[0][2] == X || board[0][2] == O){
+      return board[0][2];
+    }
   }
   for(int i = 0; i < BOARDSIZE; i++){
     for(int j = 0; j < BOARDSIZE; j++){
